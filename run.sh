444f5f4a0226902caf9729037ed4f74e8f638c7d
Jaic1 <506933131@qq.com>
2019-09-14 16:02:35 +0800

    change database

--- run.sh
+++ run.sh
@@ -8,8 +8,8 @@ XUNFENG_DB=/var/lib/mongodb
 [ ! -d $XUNFENG_LOG ] && mkdir -p ${XUNFENG_LOG}
 [ ! -d $XUNFENG_DB ] && mkdir -p ${XUNFENG_DB}
 
-nohup mongod --port 65521 --dbpath=${XUNFENG_DB} --auth  > ${XUNFENG_LOG}/db.log &
-nohup python web.py  > ${XUNFENG_LOG}/web.log &
-nohup python aider/aider.py > ${XUNFENG_LOG}/aider.log &
-nohup python nascan/nascan.py > ${XUNFENG_LOG}/scan.log &
-nohup python vulscan/vulscan.py > ${XUNFENG_LOG}/vul.log &
+nohup mongod --port 27017 --dbpath=${XUNFENG_DB} --auth  > ${XUNFENG_LOG}/db.log 2>&1 &
+nohup python web.py  > ${XUNFENG_LOG}/web.log 2>&1 &
+nohup python aider/aider.py > ${XUNFENG_LOG}/aider.log 2>&1 &
+nohup python nascan/nascan.py > ${XUNFENG_LOG}/scan.log 2>&1 &
+nohup python vulscan/vulscan.py > ${XUNFENG_LOG}/vul.log 2>&1 &
